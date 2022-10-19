e3d1325908cae7b588d8e7e978c0c5772435d5b7
Kayla Simpson <kns21a@fsu.edu>
2022-10-19 14:17:10 -0400

    test

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
+let canvasWidth = 400;
+let canvasHeight = 400;
+let fillColor = "black"
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(canvasWidth, canvasHeight);
+ 
 }
 
 function draw() {
-  background(125);
+  background((125));
+  let n = 0;
+  for (let n=0; n<6; n++){
+rect(40*n, mouseY, 50, 100); 
+
 }
+}
+  
+
